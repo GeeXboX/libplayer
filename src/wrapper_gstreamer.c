eff689f5568571a9a63daed0e6e5986c201eaafe
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2008-07-28 12:44:32 +0200

    fix gstreamer wrapper to use the internal function

--- src/wrapper_gstreamer.c
+++ src/wrapper_gstreamer.c
@@ -220,7 +220,7 @@ gstreamer_player_playback_start (player_t *player)
   g = (gstreamer_player_t *) player->priv;
   memset (mrl, '\0', PATH_MAX + 16);
 
-  switch (mrl_get_resource (player, NULL))
+  switch (mrl_sv_get_resource (player, NULL))
   {
   case MRL_RESOURCE_FILE:
   {
