4ea7a8c5bcf53e8670a8da3c32da5780343af941
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2008-09-04 20:09:51 +0200

    warn the supervisor if an event is sent to the frontend, only when the callback exists

--- src/player.c
+++ src/player.c
@@ -64,13 +64,14 @@ player_event_cb (void *data, int e, void *data_cb)
     return -1;
   }
 
-  supervisor_callback_in (player, pthread_self ());
-
   /* send to the frontend event callback */
   if (player->event_cb)
+  {
+    supervisor_callback_in (player, pthread_self ());
     res = player->event_cb (e, data_cb);
 
   supervisor_callback_out (player);
+  }
 
   if (e == PLAYER_EVENT_PLAYBACK_FINISHED)
   {
