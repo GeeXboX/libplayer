9fd2b7960f00053933abc82110cfe78f3b1524c8
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2009-12-30 16:03:48 +0100

    now that xcb is available, we can add the support for vdpau in the xine wrapper

--- src/wrapper_xine.c
+++ src/wrapper_xine.c
@@ -566,6 +566,11 @@ xine_player_init (player_t *player)
     use_x11 = 1;
     id_vo = "opengl";
     break;
+
+  case PLAYER_VO_VDPAU:
+    use_x11 = 1;
+    id_vo = "vdpau";
+    break;
 #endif /* USE_X11 */
 
   case PLAYER_VO_FB:
