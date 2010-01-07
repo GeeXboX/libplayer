aa4cb4d28609cc8def91e1063ef98fb89e7f7cb0
Mathieu Schroeter <mathieu.schroeter@mycable.ch>
2010-01-07 15:10:45 +0100

    init visual to 0 by default (root visual)

--- src/x11_common.c
+++ src/x11_common.c
@@ -481,7 +481,7 @@ pl_x11_init (player_t *player)
 {
   x11_t *x11 = NULL;
   xcb_window_t win_root;
-  xcb_visualid_t visual;
+  xcb_visualid_t visual = { 0 };
   uint32_t attributes[] = { 0, 1 }; /* black_pixel, override_redirect */
 
   if (!player)
