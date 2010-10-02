d533e7bbf2123453f149f05ac2c455486632ab5b
Davide Cavalca <davide@geexbox.org>
2010-10-02 13:19:07 +0200

    add directfb vo to libplayer-test

--- libplayer-test.c
+++ libplayer-test.c
@@ -46,7 +46,7 @@
   " -h --help               this help\n" \
   " -p --player <player>    specify the player (mplayer|xine|vlc|gstreamer)\n" \
   " -a --audio  <audioout>  specify the audio output (alsa|oss|pulse|null)\n" \
-  " -g --video  <videoout>  specify the video output (x11|sdl:x11|xv|gl|vdpau|fb|omap|vaapi|null)\n" \
+  " -g --video  <videoout>  specify the video output (x11|sdl:x11|xv|gl|vdpau|fb|directfb|omap|vaapi|null)\n" \
   " -q --quality <level>    specify the picture quality (0|1|2, best to worse)\n" \
   " -v --verbose            increase verbosity\n" \
   "\n" \
@@ -916,6 +916,8 @@ main (int argc, char **argv)
         vo = PLAYER_VO_VDPAU;
       else if (!strcmp (optarg, "fb"))
         vo = PLAYER_VO_FB;
+      else if (!strcmp (optarg, "directfb"))
+        vo = PLAYER_VO_DIRECTFB;
       else if (!strcmp (optarg, "omap"))
         vo = PLAYER_VO_OMAP;
       else if (!strcmp (optarg, "vaapi"))
